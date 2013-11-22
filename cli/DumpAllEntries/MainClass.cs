5e03bf41be97ebec9a72295a0ccc3cc9149217e1
smdn <smdn@smdn.jp>
2013-11-22 18:41:45 +0900

    added support formats of Movable Type and Hatena Diary

--- cli/DumpAllEntries/MainClass.cs
+++ cli/DumpAllEntries/MainClass.cs
@@ -23,19 +23,29 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 using System.Xml;
 
 using Smdn.Xml;
 
 namespace Smdn.Applications.HatenaBlogTools {
   class MainClass {
+    private enum OutputFormat {
+      Default,
+      MovableType,
+      HatenaDiary,
+    }
+
     public static void Main(string[] args)
     {
       string hatenaId = null;
       string blogId = null;
       string apiKey = null;
+      OutputFormat outputFormat = OutputFormat.Default;
       string outputFile = "-";
 
       for (var i = 0; i < args.Length; i++) {
@@ -50,6 +60,22 @@ namespace Smdn.Applications.HatenaBlogTools {
 
           case "-apikey":
             apiKey = args[++i];
+            break;
+
+          case "-format":
+            var format = args[++i];
+
+            switch (format) {
+              case "hatena":
+                outputFormat = OutputFormat.HatenaDiary;
+                break;
+              case "mt":
+                outputFormat = OutputFormat.MovableType;
+                break;
+              default:
+                Usage("unsupported format: {0}", format);
+                break;
+            }
             break;
 
           case "/help":
@@ -121,8 +147,129 @@ namespace Smdn.Applications.HatenaBlogTools {
       using (var outputStream = outputFile == "-"
              ? Console.OpenStandardOutput()
              : new FileStream(outputFile, FileMode.Create, FileAccess.Write)) {
-        outputDocument.Save(outputStream);
+
+        switch (outputFormat) {
+          case OutputFormat.HatenaDiary:
+            SaveAsHatenaDiary(outputDocument, outputStream);
+            break;
+
+          case OutputFormat.MovableType:
+            SaveAsMovableType(outputDocument, outputStream);
+            break;
+
+          case OutputFormat.Default:
+          default:
+            outputDocument.Save(outputStream);
+            break;
+        }
       }
+    }
+
+    private static void SaveAsMovableType(XmlDocument document, Stream outputStream)
+    {
+      /*
+       * http://www.movabletype.jp/documentation/appendices/import-export-format.html
+       */
+      var writer = new StreamWriter(outputStream, Encoding.UTF8);
+
+      writer.NewLine = "\n"; // ???
+
+      var nsmgr = new XmlNamespaceManager(document.NameTable);
+
+      nsmgr.AddNamespace("atom", Namespaces.Atom);
+      nsmgr.AddNamespace("app", Namespaces.App);
+      nsmgr.AddNamespace("hatena", Namespaces.Hatena);
+
+      foreach (XmlNode entry in document.SelectNodes("/atom:feed/atom:entry", nsmgr)) {
+        /*
+         * metadata seciton
+         */
+        writer.WriteLine(string.Concat("AUTHOR: ", entry.GetSingleNodeValueOf("atom:author/atom:name/text()", nsmgr)));
+        writer.WriteLine(string.Concat("TITLE: ", entry.GetSingleNodeValueOf("atom:title/text()", nsmgr)));
+        writer.WriteLine(string.Concat("STATUS: ", entry.GetSingleNodeValueOf("app:control/app:draft/text()", nsmgr) == "yes" ? "Draft" : "Publish"));
+
+        var updatedDate = DateTimeOffset.Parse(entry.GetSingleNodeValueOf("atom:updated/text()", nsmgr));
+
+        writer.WriteLine(string.Concat("DATE: ", updatedDate.ToString("MM/dd/yyyy hh\\:mm\\:ss tt", System.Globalization.CultureInfo.InvariantCulture)));
+
+        var tags = entry.GetNodeValuesOf("atom:category/@term", nsmgr)
+                        .Select(tag => tag.Contains(" ") ? string.Concat("\"", tag, "\"") : tag);
+
+        writer.WriteLine(string.Concat("TAGS: ", string.Join(",", tags)));
+
+        /*
+         * multiline field seciton
+         */
+        const string multilineFieldDelimiter = "-----";
+
+        writer.WriteLine(multilineFieldDelimiter);
+
+        writer.WriteLine("BODY:");
+        //writer.WriteLine(entry.GetSingleNodeValueOf("atom:content/text()", nsmgr));
+        writer.WriteLine( entry.GetSingleNodeValueOf("hatena:formatted-content/text()", nsmgr));
+        writer.WriteLine(multilineFieldDelimiter);
+
+        // end of entry
+        const string entryDelimiter = "--------";
+
+        writer.WriteLine(entryDelimiter);
+      }
+
+      writer.Flush();
+    }
+
+    private static void SaveAsHatenaDiary(XmlDocument document, Stream outputStream)
+    {
+      var nsmgr = new XmlNamespaceManager(document.NameTable);
+
+      nsmgr.AddNamespace("atom", Namespaces.Atom);
+      nsmgr.AddNamespace("app", Namespaces.App);
+      nsmgr.AddNamespace("hatena", Namespaces.Hatena);
+
+      var outputDocument = new XmlDocument();
+
+      outputDocument.AppendChild(outputDocument.CreateXmlDeclaration("1.0", "UTF-8", null));
+
+      var diaryElement = outputDocument.AppendChild(outputDocument.CreateElement("diary"));
+      var dayElements = new Dictionary<string, XmlElement>();
+
+      foreach (XmlNode entry in document.SelectNodes("/atom:feed/atom:entry", nsmgr)) {
+        var updatedDate = DateTimeOffset.Parse(entry.GetSingleNodeValueOf("atom:updated/text()", nsmgr));
+        var date = updatedDate.ToLocalTime().DateTime.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+        XmlElement dayElement, bodyElement;
+
+        if (dayElements.TryGetValue(date, out dayElement)) {
+          bodyElement = (XmlElement)dayElement.FirstChild;
+        }
+        else {
+          dayElement = (XmlElement)diaryElement.AppendChild(outputDocument.CreateElement("day"));
+
+          dayElement.SetAttribute("date", date);
+          dayElement.SetAttribute("title", string.Empty);
+
+          bodyElement = (XmlElement)dayElement.AppendChild(outputDocument.CreateElement("body"));
+
+          dayElements[date] = dayElement;
+        }
+
+        var body = new StringBuilder();
+
+        body.AppendFormat("*{0}*", UnixTimeStamp.ToInt64(updatedDate));
+
+        var joinedCategory = string.Join("][", entry.GetNodeValuesOf("atom:category/@term", nsmgr));
+
+        if (0 < joinedCategory.Length)
+          body.AppendFormat("[{0}]", joinedCategory);
+
+        body.AppendLine(entry.GetSingleNodeValueOf("atom:title/text()", nsmgr));
+
+        body.AppendLine(entry.GetSingleNodeValueOf("atom:content/text()", nsmgr));
+        body.AppendLine();
+
+        bodyElement.AppendText(body.ToString());
+      }
+
+      outputDocument.Save(outputStream);
     }
 
     private static void Usage(string format, params string[] args)
@@ -133,7 +280,7 @@ namespace Smdn.Applications.HatenaBlogTools {
       }
 
       Console.Error.WriteLine("usage:");
-      Console.Error.WriteLine("  {0} -id <hatena-id> -blogid <blog-id> -apikey <api-key> [outfile]",
+      Console.Error.WriteLine("  {0} -id <hatena-id> -blogid <blog-id> -apikey <api-key> [-format (hatena|mt)] [outfile]",
                               System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location));
 
       Environment.Exit(-1);
