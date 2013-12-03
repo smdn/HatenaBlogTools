9a91daf5a35b971bbda3aa7579d6a6e08b4f2e77
smdn <smdn@smdn.jp>
2013-12-03 23:15:27 +0900

    updated assembly info

--- cli/CommonAssemblyInfo.cs
+++ cli/CommonAssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.01.*")]
+[assembly: AssemblyVersion("1.02.*")]
