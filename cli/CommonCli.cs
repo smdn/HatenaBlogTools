77f3dc721d94730de339ccdc4cfc511f2090a258
smdn <smdn@smdn.jp>
2018-04-16 22:08:55 +0900

    removed redundant using statement

--- cli/CommonCli.cs
+++ cli/CommonCli.cs
@@ -26,7 +26,6 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
-using System.Xml.Linq;
 
 namespace Smdn.Applications.HatenaBlogTools {
   partial class MainClass {
