d4c3af061a184190e52722e9d4e2319b0d05d399
smdn <smdn@smdn.jp>
2018-04-19 06:20:03 +0900

    removed redundant using statements

--- cli/ReplaceContentText/MainClass.cs
+++ cli/ReplaceContentText/MainClass.cs
@@ -24,15 +24,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-
-using Smdn.Xml;
 
 namespace Smdn.Applications.HatenaBlogTools {
   partial class MainClass {
