6f9e00435411d4589142172f3cdd73d522403abb
smdn <smdn@smdn.jp>
2018-05-19 19:21:37 +0900

    added option '--from-atom-file'

--- cli/PostNewEntry/Main.cs
+++ cli/PostNewEntry/Main.cs
@@ -50,6 +50,12 @@ namespace Smdn.Applications.HatenaBlogTools {
       yield return "<本文>                 : 投稿する記事の本文を指定します";
       yield return "--from-file <ファイル> : 指定された<ファイル>の内容を本文として投稿します";
       yield return "--from-file -          : 標準入力に与えられた内容を本文として投稿します";
+      yield return "";
+      yield return "--from-atom-file <ファイル> : Atomフィード形式のファイルに記載されている内容で投稿います";
+      yield return "                              複数のエントリが記載されている場合は、それをすべて投稿します";
+      yield return "                              既存のエントリの場合(メンバURIが定義されている場合)はエントリを更新します";
+      yield return "                              このオプションを指定した場合、ほかのオプションによる投稿内容の";
+      yield return "                              指定はすべて無視されます(--title, --from-fileなど)";
     }
 
     public void Run(string[] args)
@@ -59,6 +65,7 @@ namespace Smdn.Applications.HatenaBlogTools {
 
       var entry = new Entry();
       string contentFile = null;
+      string atomEntriesFile = null;
 
       for (var i = 0; i < args.Length; i++) {
         switch (args[i]) {
@@ -82,57 +89,116 @@ namespace Smdn.Applications.HatenaBlogTools {
             contentFile = args[++i];
             break;
 
+          case "--from-atom-file":
+            atomEntriesFile = args[++i];
+            break;
+
           default:
             entry.Content = args[i];
             break;
         }
       }
 
-      if (!string.IsNullOrEmpty(contentFile)) {
-        if (contentFile == "-") {
-          using (var reader = new StreamReader(Console.OpenStandardInput())) {
-            entry.Content = reader.ReadToEnd();
+      var postingEntries = new List<Entry>();
+
+      if (string.IsNullOrEmpty(atomEntriesFile)) {
+        if (!string.IsNullOrEmpty(contentFile)) {
+          if (contentFile == "-") {
+            using (var reader = new StreamReader(Console.OpenStandardInput())) {
+              entry.Content = reader.ReadToEnd();
+            }
+          }
+          else if (File.Exists(contentFile)) {
+            entry.Content = File.ReadAllText(contentFile);
+          }
+          else {
+            Usage("ファイル '{0}' が見つかりません", contentFile);
           }
         }
-        else if (File.Exists(contentFile)) {
-          entry.Content = File.ReadAllText(contentFile);
+
+        postingEntries.Add(entry);
+      }
+      else {
+        using (var reader = File.OpenText(atomEntriesFile)) {
+          foreach (var e in HatenaBlogAtomPubClient.ReadEntriesFrom(XDocument.Load(reader))) {
+            if (e.MemberUri == null)
+              postingEntries.Add(new Entry(e)); // post as new entry instead of update existing entry
+            else
+              postingEntries.Add(e);
+          }
         }
-        else {
-          Usage("ファイル '{0}' が見つかりません", contentFile);
-        }
+
+        postingEntries.Sort((e1, e2) => DateTimeOffset.Compare(e1.Updated ?? DateTimeOffset.MinValue, e2.Updated ?? DateTimeOffset.MinValue));
       }
 
       if (!Login(credential, out HatenaBlogAtomPubClient hatenaBlog))
         return;
 
-      Console.Write("投稿しています ... ");
+      var failedEntries = new List<Entry>();
 
       try {
-        var statusCode = hatenaBlog.PostEntry(entry, out XDocument responseDocument);
+        foreach (var postingEntry in postingEntries) {
+          try {
+            HttpStatusCode statusCode;
+            HttpStatusCode expectedStatusCode;
+            XDocument responseDocument;
 
-        if (statusCode == HttpStatusCode.Created) {
-          var createdUri = responseDocument.Element(AtomPub.Namespaces.Atom + "entry")
-                                           ?.Elements(AtomPub.Namespaces.Atom + "link")
-                                           ?.FirstOrDefault(e => e.HasAttributeWithValue("rel", "alternate"))
-                                           ?.GetAttributeValue("href");
+            if (postingEntry is PostedEntry updatingEntry) {
+              Console.Write($"更新しています ({updatingEntry.MemberUri} '{updatingEntry.Title}') ... ");
 
-          Console.ForegroundColor = ConsoleColor.Green;
-          Console.WriteLine("完了しました: {0}", createdUri);
-          Console.ResetColor();
-        }
-        else {
-          Console.ForegroundColor = ConsoleColor.Red;
-          Console.Error.WriteLine("失敗しました: {0}", statusCode);
-          Console.ResetColor();
+              expectedStatusCode = HttpStatusCode.OK;
+              statusCode = hatenaBlog.UpdateEntry(updatingEntry, out responseDocument);
+            }
+            else {
+              Console.Write($"投稿しています ('{postingEntry.Title}') ... ");
+
+              expectedStatusCode = HttpStatusCode.Created;
+              statusCode = hatenaBlog.PostEntry(postingEntry, out responseDocument);
+            }
+
+            if (statusCode == expectedStatusCode) {
+              var createdUri = responseDocument.Element(AtomPub.Namespaces.Atom + "entry")
+                                               ?.Elements(AtomPub.Namespaces.Atom + "link")
+                                               ?.FirstOrDefault(e => e.HasAttributeWithValue("rel", "alternate"))
+                                               ?.GetAttributeValue("href");
+
+              Console.ForegroundColor = ConsoleColor.Green;
+              Console.WriteLine("完了しました: {0}", createdUri);
+              Console.ResetColor();
+            }
+            else {
+              Console.ForegroundColor = ConsoleColor.Red;
+              Console.Error.WriteLine("失敗しました: {0}", statusCode);
+              Console.ResetColor();
+
+              failedEntries.Add(postingEntry);
+            }
+          }
+          catch (PostEntryFailedException ex) {
+            Console.WriteLine();
+            Console.Error.WriteLine(ex);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("失敗しました");
+            Console.ResetColor();
+
+            failedEntries.Add(postingEntry);
+          }
         }
       }
-      catch (PostEntryFailedException ex) {
-        Console.WriteLine();
-        Console.Error.WriteLine(ex);
+      finally {
+        if (1 < postingEntries.Count && 0 < failedEntries.Count) {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("下記エントリの投稿または更新に失敗しました");
+          Console.ResetColor();
 
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.Error.WriteLine("失敗しました");
-        Console.ResetColor();
+          foreach (var failedEntry in failedEntries) {
+            if (failedEntry is PostedEntry e)
+              Console.WriteLine($"{e.MemberUri} '{e.Title}'");
+            else
+              Console.WriteLine(failedEntry.Title);
+          }
+        }
       }
     }
   }
