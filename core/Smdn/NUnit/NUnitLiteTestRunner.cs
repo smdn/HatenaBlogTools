d795e8ec5f711caa808f409fd9a30c9cada451c3
smdn <smdn@smdn.jp>
2020-03-28 20:35:09 +0900

    import from libs/Smdn

---
+++ core/Smdn/NUnit/NUnitLiteTestRunner.cs
@@ -0,0 +1,10 @@
+using NUnitLite;
+using System.Reflection;
+
+public class NUnitLiteTestRunner {
+  static int Main(string[] args) {
+    var typeInfo = typeof(NUnitLiteTestRunner).GetTypeInfo();
+
+    return (new AutoRun(typeInfo.Assembly)).Execute(args);
+  }
+}
